8a0bfdf4c35897addc6c2dd6d84a190b14f863a0
leeboyjcc <44202385+leeboyjcc@users.noreply.github.com>
2018-11-08 12:34:56 +0800

    Update ShiyanlouGithubSpider.py

--- shiyanlougithub/ShiyanlouGithubSpider.py
+++ shiyanlougithub/ShiyanlouGithubSpider.py
@@ -15,7 +15,7 @@ class ShiyanlouGithubSpider(scrapy.Spider):
         return url_tmpl
 
     def parse(self, response):
-        for repos in response.css('div#user-repositories-list li.col-12.d-block.width-full.py-4.border-bottom.public.source'):
+        for repos in response.css('div#user-repositories-list li'):
             yield {
                     'name': repos.css('div.d-inline-block.mb-1 h3 a::text').re_first('\S+'),
                     'update_time': repos.css('div.f6.text-gray.mt-2 relative-time::attr(datetime)').extract_first()
